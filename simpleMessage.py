684c1378ad9a2870edf00617df76cd306c8d1ae4
thomas tourrette <ttourrette@gmail.com>
2018-12-20 01:03:48 +0100

    Exercise on string and numbers

---
+++ simpleMessage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env
+
+message = "Salut toi"
+print(message)
+message = "Hey !"
+print(message)
+
+programmer = "ada lovelace"
+programmer = programmer.title()
+print(programmer)
+
+
+
+message = "Hello " + programmer + ' how are "you"?'
+print(message)
+
+
+age = 30
+age2 = 27
+
+# Commentaire useless
+print("Salut j'ai %d ans, et toi tu as %d ans."% (age, age2))
