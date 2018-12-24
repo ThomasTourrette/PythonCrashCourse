debabe5a2cf732fdbca7d749728f315b01323e2d
thomas tourrette <ttourrette@gmail.com>
2018-12-25 00:34:06 +0100

    Tests on while and input()

---
+++ whileTest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env
+
+## Input
+#message = input("Tell me something, and I will repeat back to you: ")
+#print(message)
+#
+## While loop
+#current_number = 1
+#while current_number <= 5:
+#    print(current_number)
+#    current_number += 1
+#
+#
+#while True:
+#    message = input("Enter 'quit' to quit the program: ")
+#    if "quit" == message:
+#        break
+
+# Using while loop with list and dictionaries
+#unconfirmed_users = ['thomas', 'john', 'laura']
+#unconfirmed_users_copy = list(unconfirmed_users)
+#confirmed_users = []
+#
+#while unconfirmed_users:
+#    message = input("Is " + str(unconfirmed_users[0]) + " a confirmed user (y/n) ? ")
+#    if ('y' == message):
+#        confirmed_users.append(unconfirmed_users[0])
+#    del unconfirmed_users[0]
+#
+#print(unconfirmed_users_copy)
+#print(confirmed_users)
+
